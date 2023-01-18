65e9233aae531a6b60e48dc0e303d3ced6374a7c
Vidit Khandelwal <viditk17@gmail.com>
2023-01-18 22:04:02 +0530

    edited loading page

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./LandingPage/LandingPage.module.css";
+import { BeatLoader } from "react-spinners";
 
 const Loading = (props: {
   children:
@@ -16,11 +17,12 @@ const Loading = (props: {
     "w-screen h-screen flex flex-col space-y-6 justify-center " + styles.sg;
 
   const mainContentStyles =
-    "w-full h-full flex flex-col bg-stone-200 dark:bg-stone-900 justify-center";
+    "w-full h-full flex flex-col space-y-1 bg-stone-200 dark:bg-stone-900 justify-center";
 
   return (
     <div className={mainContainerStyles}>
       <div className={mainContentStyles}>
+      <BeatLoader className="self-center" color="#d97706" />
         <p className="text-amber-600 text-4xl self-center font-bold">
           {props.children}
         </p>
