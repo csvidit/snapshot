d34fdd5a95ae6645a5fadd2478825a9a3aa2bffd
Vidit Khandelwal <viditk17@gmail.com>
2023-01-22 00:00:06 +0530

    removed typeerror issue during build

--- src/components/HomePage/News/StoryCard.tsx
+++ src/components/HomePage/News/StoryCard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BsDot } from "react-icons/bs";
 import Link from "next/link";
 
-const StoryCard = (props) => {
+const StoryCard = (props: { title: string | number | boolean | React.ReactFragment | React.ReactPortal | React.ReactElement<any, string | React.JSXElementConstructor<any>> | null | undefined; source: string | number | boolean | React.ReactFragment | React.ReactPortal | React.ReactElement<any, string | React.JSXElementConstructor<any>> | null | undefined; time: string | number | Date; }) => {
   return (
     <Link href="#" className="flex flex-col space-y-1 hover:bg-slate-800 p-2 rounded-xl col-span-2 font-light h-max">
       <div className="flex flex-col space-y-1 ">
